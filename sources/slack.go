37fcd525d28afe599aa2afb462297f6161a0887a
utahkaA <utahka.akiba@gmail.com>
2018-01-18 20:51:38 +0900

    added search function to slack.go

--- sources/slack.go
+++ sources/slack.go
@@ -11,6 +11,11 @@ import (
   "github.com/spf13/viper"
 )
 
+type SlackChannel struct {
+  id    string  `json:"id"`
+  Name  string  `json:"name"`
+}
+
 type SlackHistory struct {
   Ok        bool            `json:"ok"`
   Messages  []SlackMessage  `json:"messages"`
@@ -18,23 +23,43 @@ type SlackHistory struct {
 }
 
 type SlackMessage struct {
-  Text      string      `json:"text"`
-  Username  string      `json:"username"`
-  BotId     string      `json:"bot_id"`
-  Type      string      `json:"type"`
-  Subtype   string      `json:"subtype`
-  Timestamp string      `json:"ts"`
-  Reactions []Reaction  `json:"reactions"`
+  Text      string            `json:"text"`
+  Username  string            `json:"username"`
+  BotId     string            `json:"bot_id"`
+  Type      string            `json:"type"`
+  Subtype   string            `json:"subtype`
+  Timestamp string            `json:"ts"`
+  Reactions []SlackReaction   `json:"reactions"`
 }
 
-type Reaction struct {
+type SlackReaction struct {
   Name  string    `json:"name"`
   Users []string  `json:"users"`
   Count int       `json:"count"`
 }
 
+type SlackSearchResult struct {
+  Ok        bool                  `json:"ok"`
+  Messages  SlackMatchMessages    `json:"messages"`
+  Query     string                `json:"query"`
+}
+
+type SlackMatchMessages struct {
+  Matches []SlackMatch  `json:"matches"`
+  Total   int           `json:"total"`
+}
+
+type SlackMatch struct {
+  SlackMessage
+  Channel       SlackChannel  `json:"channel"`
+  Permalink     string        `json:"permalink"`
+}
+
 type SlackInterface struct {
-  config  *viper.Viper
+  config      *viper.Viper
+  token       string
+  channelId   string
+  channel     string
 }
 
 func NewSlackInterface(configPath string) (*SlackInterface, error) {
@@ -50,6 +75,10 @@ func NewSlackInterface(configPath string) (*SlackInterface, error) {
   }
   sl.config = v
 
+  sl.token = sl.config.GetString("token")
+  sl.channelId = sl.config.GetString("channel_id")
+  sl.channel = sl.config.GetString("channel")
+
   return sl, nil
 }
 
@@ -57,11 +86,11 @@ func (sl *SlackInterface) constructEndpoint(apiMethod string) (string, error) {
   endpointTemp := sl.config.GetString(apiMethod)
 
   getQuery := url.Values{}
-  getQuery.Set("token", sl.config.GetString("token"))
+  getQuery.Set("token", sl.token)
 
   switch apiMethod {
   case "api.history":
-    getQuery.Add("channel", sl.config.GetString("channel_id"))
+    getQuery.Add("channel", sl.channelId)
     return fmt.Sprintf("%s?%s", endpointTemp, bytes.NewBufferString(getQuery.Encode())), nil
   case "api.search":
     return fmt.Sprintf("%s?%s", endpointTemp, bytes.NewBufferString(getQuery.Encode())), nil
@@ -83,9 +112,7 @@ func (sl *SlackInterface) constructSlackSearchURL(query string) string {
   return url
 }
 
-func (sl *SlackInterface) GetSlackHistory() (*SlackHistory, error) {
-  url := sl.constructSlackHistoryURL()
-
+func (sl *SlackInterface) getRequest(url string) ([]byte, error) {
   req, err := http.NewRequest("GET", url, nil)
   if err != nil {
     errMsg := "[Error] Creating a new request to Slack failed\n"
@@ -100,10 +127,40 @@ func (sl *SlackInterface) GetSlackHistory() (*SlackHistory, error) {
   }
   defer resp.Body.Close()
 
-  history, err := ioutil.ReadAll(resp.Body)
+  body, err := ioutil.ReadAll(resp.Body)
   if err != nil {
     errMsg := "[Error] A ploblem occurred while reading a body of the response.: %s\n"
     return nil, fmt.Errorf(errMsg, err)
+  }
+  return body, nil
+}
+
+func (sl *SlackInterface) GetSlackHistory() (*SlackHistory, error) {
+  url := sl.constructSlackHistoryURL()
+
+  // req, err := http.NewRequest("GET", url, nil)
+  // if err != nil {
+  //   errMsg := "[Error] Creating a new request to Slack failed\n"
+  //   return nil, fmt.Errorf(errMsg)
+  // }
+  //
+  // client := new(http.Client)
+  // resp, err := client.Do(req)
+  // if err != nil {
+  //   errMsg := "[Error] GET request to Slack failed (send request)\n"
+  //   return nil, fmt.Errorf(errMsg)
+  // }
+  // defer resp.Body.Close()
+  //
+  // history, err := ioutil.ReadAll(resp.Body)
+  // if err != nil {
+  //   errMsg := "[Error] A ploblem occurred while reading a body of the response.: %s\n"
+  //   return nil, fmt.Errorf(errMsg, err)
+  // }
+  history, err := sl.getRequest(url)
+  if err != nil {
+    errMsg := "[Error] GET request to %s falid :\n\t%s"
+    return nil, fmt.Errorf(errMsg, url, err)
   }
 
   var slackHist SlackHistory
@@ -116,8 +173,43 @@ func (sl *SlackInterface) GetSlackHistory() (*SlackHistory, error) {
   return &slackHist, nil
 }
 
-func (sl *SlackInterface) DecimationSlackHistory(query string) error {
+func (sl *SlackInterface) Search(channel string, query string) (*SlackSearchResult, error) {
   url := sl.constructSlackSearchURL(query)
-  fmt.Println(url)
+
+  matches, err := sl.getRequest(url)
+  if err != nil {
+    errMsg := "[Error] GET request to %s falid :\n\t%s"
+    return nil, fmt.Errorf(errMsg, url, err)
+  }
+
+  var result SlackSearchResult
+  err = json.Unmarshal(matches, &result)
+  if err != nil {
+    errMsg := "[Error] A problem occurred while parsing data as JSON file.: %s\n"
+    return nil, fmt.Errorf(errMsg, err)
+  }
+
+  slackMatches := make([]SlackMatch, 0)
+  for _, match := range result.Messages.Matches {
+    if match.Channel.Name == channel {
+      slackMatches = append(slackMatches, match)
+    }
+  }
+  result.Messages.Matches = slackMatches
+
+  return &result, nil
+}
+
+func (sl *SlackInterface) DecimationSlackHistory(query string) error {
+  result, err := sl.Search(sl.channel, query)
+  if err != nil {
+    errMsg := "[Error] A problem occurred while searching: %s\n"
+    return fmt.Errorf(errMsg, err)
+  }
+
+  for i, match := range result.Messages.Matches {
+    fmt.Println(i, match.Text)
+  }
+
   return nil
 }
--- sources/slack_test.go
+++ sources/slack_test.go
@@ -33,15 +33,27 @@ func TestConstructEndpoint(t *testing.T) {
     t.Fatalf("Faild Test: ConstructEndpoint()")
   }
 
+  // check about not exist argument
   url, err := slackInterface.constructEndpoint("hogehoge")
   if err != nil {
     t.Log(err)
   }
   t.Logf("url: %s", url)
 
+  // check about exist argument
   url, err = slackInterface.constructEndpoint("api.history")
   if err != nil {
     t.Log(err)
   }
   t.Logf("url: %s", url)
+}
+
+func TestSearch(t *testing.T) {
+  configPath := utils.RaccoltaHome()
+  slackInterface, err := NewSlackInterface(configPath)
+  if err != nil {
+    t.Fatalf("Faild Test: Search()")
+  }
+
+  slackInterface.Search("store_raccolta", "on:2018-01-16")
 }
